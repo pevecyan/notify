bc593b591687427236ddd92fa2ca65bc3ee36ea3
Žan Pevec <pevecyan@outlook.com>
2018-03-12 10:51:46 +0100

    Soem changes

--- notify.go
+++ notify.go
@@ -15,6 +15,8 @@ type Notify struct {
 	Next plugin.Handler
 }
 
+var output io.Writer = os.Stdout
+
 func (e Notify) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
 	// a slightly more complex example as to make this more interesting.
@@ -22,7 +24,7 @@ func (e Notify) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) 
 	// answer comes back, it will print "example".
 
 	// Wrap.
-	fmt.Println("notify")
+	fmt.Println(output, "notify")
 
 	// Call next plugin (if any).
 	return plugin.NextOrFailure(e.Name(), e.Next, ctx, w, r)
@@ -30,27 +32,3 @@ func (e Notify) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) 
 
 // Name implements the Handler interface.
 func (e Notify) Name() string { return "notify" }
-
-// ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when
-// WriteMsg is called.
-type ResponsePrinter struct {
-	dns.ResponseWriter
-}
-
-// NewResponsePrinter returns ResponseWriter.
-func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
-	return &ResponsePrinter{ResponseWriter: w}
-}
-
-// WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard
-// output.
-func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	fmt.Fprintln(out, ex)
-	return r.ResponseWriter.WriteMsg(res)
-}
-
-// Make out a reference to os.Stdout so we can easily overwrite it for testing.
-var out io.Writer = os.Stdout
-
-// What we will print.
-const ex = "notify"
